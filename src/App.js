40183b44b82db61f067cad2a93759749ed6ace74
gxPan <gxpan1006@gmail.com>
2023-06-04 14:36:14 +0800

    [Tech] link with backend

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:8080/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get started
         </a>
       </header>
     </div>
