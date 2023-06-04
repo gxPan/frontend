be2a30040a3362af2b93a788297139b6d1689fcd
gxPan <gxpan1006@gmail.com>
2023-06-04 18:05:13 +0800

    [Tech] get and display weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:8080/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get started
-        </a>
-      </header>
-    </div>
-  );
+    const [weatherData, setWeatherData] = useState(null);
+
+    useEffect(() => {
+      fetch('http://localhost:8080/weather')
+          .then(response => response.json())
+          .then(data => setWeatherData(data));
+    }, []);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="http://localhost:8080/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Get started
+          </a>
+        {weatherData && (
+            <div className="Weather">
+              <h2>Weather Information</h2>
+              <p>Location: {weatherData.location.name}</p>
+              <p>Temperature: {weatherData.current.temp_c} °C</p>
+              <p>Condition: {weatherData.current.condition.text}</p>
+              {/* Can display more data according to demands */}
+            </div>
+        )}
+        </header>
+      </div>
+    );
 }
 
 export default App;
